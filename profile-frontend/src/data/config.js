58053d9a4a78613eef39b4637039763f7c77c68c
Shreyas Patil <shreyas42000@gmail.com>
2021-01-18 12:29:15 +0000

    first commit

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Shreyas Patil",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  greetingDescription: "I'm Shreyas and I'm a Dev!",
+  githubUrl: "https://github.com/TheGreatFellow",
+  linkedinUrl: "https://www.linkedin.com/in/shreyas-patil-521789191/",
+  cvLink: "http://www.blankwebsite.com/",
 };
